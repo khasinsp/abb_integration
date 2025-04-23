e813e143a295ae7ac5d3ebe3b38a506946b8dd5f
Narek Bayanduryan <karim.hassan@inspire.ch>
2025-04-23 12:57:50 +0200

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,132 +1,181 @@
 #include <bits/stdc++.h>
+#include <sys/syscall.h>
+#include <linux/sched.h>
 #include "../include/tcp_server.h"
-#include "../include/command.h"
 
 #define LOCAL_HOST "192.168.0.60"
-#define LOCAL_PORT_RX 5001
-#define LOCAL_PORT_TX 5000
+#define LOCAL_PORT 6008
 
-#define ANGLE_THRESHOLD 5
-#define SPEED_THRESHOLD 10
-#define FREQ 100
+#define numJoints 6 //Robot has 6 joints
 
-struct RobotState {
-    float react_timestamp;
-    uint32_t queue_size;
-    float values[6];
+#define ANGLE_THRESHOLD 360 //Threshold the command angles
+#define SPEED_THRESHOLD 360 //Threshold the speed
+
+#define FREQ 20.0
+
+/*
+Set FIFO Policy with priority for the current Thread
+*/
+void set_realtime_priority(int priority) {
+    struct sched_param param;
+    param.sched_priority = priority;
+
+    if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &param) != 0) {
+        perror("Unable to set realtime priority");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+Set CPU Affinity for the current Thread
+*/
+void set_CPU(size_t cpu)
+{
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+
+    CPU_SET(cpu, &mask);
+
+    // pid = 0 means "calling process"
+    if (sched_setaffinity(0, sizeof(mask), &mask) == -1)
+    {
+        std::cerr << "Error setting CPU affinity: "
+                  << std::strerror(errno) << std::endl;
+    }
+}
+
+/*
+SIGXCPU Handler
+*/
+void sigxcpu_handler(int signum) {
+    std::cerr << "Runtime overrun detected: Task exceeded allocated runtime" << std::endl;
+}
+/*
+Signal Handler for handling runtime overruns
+*/
+void setup_signal_handler() {
+    struct sigaction sa;
+    sa.sa_handler = sigxcpu_handler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGXCPU, &sa, NULL) != 0) {
+        perror("Failed to set SIGXCPU handler");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+Function for setting Deadline Policy to current Thread
+*/
+struct sched_attr {
+    uint32_t size;
+    uint32_t sched_policy;
+    uint64_t sched_flags;
+    int32_t sched_nice;
+    uint32_t sched_priority;
+    uint64_t sched_runtime;
+    uint64_t sched_deadline;
+    uint64_t sched_period;
 };
+void set_realtime_deadline(unsigned long runtime, unsigned long deadline, unsigned long period)
+{
+    struct sched_attr attr;
+    int ret;
 
+    // Zero out the structure
+    memset(&attr, 0, sizeof(attr));
+
+    // Set the scheduling policy to SCHED_DEADLINE
+    attr.size = sizeof(attr);
+    attr.sched_policy = SCHED_DEADLINE;
+    attr.sched_runtime = runtime;
+    attr.sched_deadline = deadline;
+    attr.sched_period = period;
+
+    // Enable Overrun detection
+    attr.sched_flags |= SCHED_FLAG_DL_OVERRUN;
+
+    // Use syscall to set the scheduling policy and parameters
+    ret = syscall(SYS_sched_setattr, gettid(), &attr, 0);
+    if (ret != 0)
+    {
+        perror("Failed to set SCHED_DEADLINE");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+RobotState for unpacking received bytes and copying them to readable joint states
+*/
+struct RobotState {
+    float values[numJoints];
+};
 RobotState unpack(const std::string& buffer) {
     RobotState state;
 
-    if (buffer.size() < sizeof(RobotState)) {
-        std::cout << "response too short" << std::endl;
-        // TODO (What should happen here?)
+    for (int i = 0; i < numJoints; ++i) {
+        std::memcpy(&state.values[i], buffer.data() + i * sizeof(float), sizeof(float));
     }
-
-    std::memcpy(&state, buffer.data(), sizeof(RobotState));
 
     return state;
 }
 
-bool check_buffer(const std::string &buffer) {
+/*
+check buffer for checking the received message
+*/
+bool check_buffer(std::string &buffer) {
     if (buffer.size() < 2) return false;
 
     return static_cast<unsigned char>(buffer[buffer.size() - 2]) == 0xAA &&
            static_cast<unsigned char>(buffer[buffer.size() - 1]) == 0x55;
 }
 
+std::queue<std::vector<float>> command_arr;
+std::vector<float> current_joints;
 
-RobotState current_pose;
-float start_delay = 0;
-float start_time;
-
-void listener_thread() {
-
-    TCPServer *socketServerRX = new TCPServer(LOCAL_HOST, LOCAL_PORT_RX);
-    socketServerRX->start_();
-
-    std::string main_buffer;
-    std::string in_buffer;
-
-    unsigned short i = 0;
-    while (true) {
-        ssize_t bytes = socketServerRX->recv_(in_buffer);
-        if (bytes < 0) {
-            continue;
-        }
-        if (bytes > 0) {
-            main_buffer += in_buffer;
-        }
-        if (bytes == 0) {
-            std::cout << "Error in recv_() in listener_thread()" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-
-        if (check_buffer(main_buffer)) {
-            current_pose = unpack(main_buffer);
-            main_buffer = "";
-            start_delay = current_pose.react_timestamp + 5;
-            std::cout << "start delay: " << start_delay << std::endl;
-            // for (int i = 0; i < 6; i++) {
-            //     std::cout << current_pose.values[i] << ", ";
-            // } 
-            // std::cout << std::endl;
-            // if (current_pose.queue_size > 0) std::cout << "queue size: " << current_pose.queue_size << std::endl;
-        }
-    }
-}
-
-void generate_motion(std::vector<std::vector<float>> &command_arr, std::vector<float> command) {
+/*
+Sets the interpolated commands into the command_arr queue
+Start Position is current joint positions
+End Position is absolute command
+*/
+void generate_motion(std::queue<std::vector<float>> &command_arr, std::vector<float> command) {
 
     // calculate length of command array given frequency and speed
     float speed = command.back();
     command.pop_back();
-    float max_angle = *std::max_element(command.begin(), command.end());
+
+    std::vector<float> current_joints_copy = current_joints;
+    std::vector<float> max_vec;
+    for (int i = 0; i < command.size(); i++) {
+        max_vec.push_back(command[i] - current_joints_copy[i]);
+    }
+    float max_angle = *std::max_element(max_vec.begin(), max_vec.end(), [](float a, float b) {return std::abs(a) < std::abs(b);});
+    max_angle = std::abs(max_angle);
+
+    float t_end = max_angle / speed;
     std::cout << "Max Angle is: " << max_angle << std::endl;
     int commandlen = max_angle / speed * FREQ;
-
-    // We want joint correction given the current joint angles
-    // first vector in command are current joint angles
-    // if (command_arr.size() == 0) {
-    //     std::vector<float> init_vec;
-    //     for (int i = 0; i < command.size(); i++) {
-    //         init_vec.push_back(current_pose.values[i]);
-    //     }
-    //     command_arr.push_back(init_vec);
-    // }
-    // // if robot moving (command array not empty) and new command given -> wait until command array is empty
-    // else {
-    //     while (command_arr.size() > 0) {
-    //         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    //     }
-    //     std::vector<float> init_vec;
-    //     for (int i = 0; i < command.size(); i++) {
-    //         init_vec.push_back(current_pose.values[i]);
-    //     }
-    //     command_arr.push_back(init_vec);
-    // }
 
     for (int i = 0; i < commandlen; i++) {
         std::vector<float> c(command.size());
         for (int j = 0; j < c.size(); j++) {
-            // c[j] = command_arr[0][j] + command[j] / (commandlen - 1) * i;
-            c[j] = command[j] / (commandlen - 1) * i;
-            // std::cout << c[j] << ", ";
+            c[j] = current_joints_copy[j] + (command[j] - current_joints_copy[j]) / (commandlen - 1) * i;
+            
         }
-        // std::cout << std::endl;
-        command_arr.push_back(c);
+        command_arr.push(c);
     }
 
     return;
 
 }
 
-std::vector<std::vector<float>> command_arr;
-
-
-
-void command_thread() {
+/*
+Thread for setting commands
+*/
+void user_input_thread() {
+    // std::this_thread::sleep_for(std::chrono::seconds(5));
+    // std::vector<float> command = {0, 0, 0, 0, 0, 0, 90};
+    // generate_motion(command_arr, command);
 
     while (true) {
         float user_input;
@@ -135,11 +184,6 @@ void command_thread() {
         for (int i = 0; i < 7; i++) {
             std::cin >> user_input;
             command.push_back(user_input);
-        }
-
-        if (command.size() != 7) {
-            std::cout << "Wrong command size, try again!" << std::endl;
-            continue;
         }
 
         bool high_angle = false;
@@ -165,32 +209,35 @@ void command_thread() {
         }
 
         generate_motion(command_arr, command);
-
-        start_time = start_delay;
     }
 }
 
-std::vector<uint8_t> generate_command(std::vector<float> command, float s) {
-    std::vector<uint8_t> buffer;
+/*
+Generates a command readable for the robot
+*/
+std::vector<int8_t> generate_command(std::vector<float> command, bool last_command) {
+    std::vector<int8_t> buffer;
     const uint8_t end_of_command[] = { 0xAA, 0x55 };
-    uint32_t last_value = 10;
-    uint8_t last_value_bytes [sizeof(uint32_t)];
-    std::memcpy(last_value_bytes, &last_value, sizeof(uint32_t));
+    int32_t last_value = last_command ? -1 : 10;
+    int8_t last_value_bytes [sizeof(int32_t)];
+    std::memcpy(last_value_bytes, &last_value, sizeof(int32_t));
+
+    float s = 0.0;
 
     // Copy Joint values to send buffer
     for (float joint : command) {
-        uint8_t bytes[sizeof(float)];
+        int8_t bytes[sizeof(float)];
         std::memcpy(bytes, &joint, sizeof(float));
         buffer.insert(buffer.end(), bytes, bytes + sizeof(float));
     }
 
     // Copy Start time to send buffer
-    uint8_t timeBytes[sizeof(float)];
+    int8_t timeBytes[sizeof(float)];
     std::memcpy(timeBytes, &s, sizeof(float));
     buffer.insert(buffer.end(), timeBytes, timeBytes + sizeof(float));
 
     // Copy last value bytes (10) to send buffer
-    buffer.insert(buffer.end(), last_value_bytes, last_value_bytes + sizeof(uint32_t));
+    buffer.insert(buffer.end(), last_value_bytes, last_value_bytes + sizeof(int32_t));
     
     // Copy End of Command bytes (0xAA, 0x55) to send buffer
     buffer.insert(buffer.end(), std::begin(end_of_command), std::end(end_of_command));
@@ -198,63 +245,104 @@ std::vector<uint8_t> generate_command(std::vector<float> command, float s) {
     return buffer;
 }
 
-void settingPose_thread() {
+/*
+Main communication Thread
+*/
+int main_thread() {
 
-    TCPServer *socketServerTX = new TCPServer(LOCAL_HOST, LOCAL_PORT_TX);
-    socketServerTX->start_();
+    set_CPU(0);
 
-    std::string main_command_buffer;
-    std::string in_command_buffer;
+    TCPServer *socketServer = new TCPServer(LOCAL_HOST, LOCAL_PORT);
 
-    const uint8_t end_of_command[] = { 0xAA, 0x55 };
-    uint32_t last_value = 10;
-    uint8_t last_value_bytes [sizeof(uint32_t)];
-    std::memcpy(last_value_bytes, &last_value, sizeof(uint32_t));
+    socketServer->start_();
+    socketServer->setToNonblockingMode_();
 
-    int i = 0;
+    std::string in_buffer;
+    std::string received_main_buffer;
+
+    std::vector<int8_t> out_buffer;
+
+    RobotState current_state;
+
+    std::vector<float> command;
+
+    float period = 1 / FREQ * 1.0e6;
+
+    set_realtime_priority(99);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    short i = 0;
     while (true) {
-        auto start = std::chrono::high_resolution_clock::now();
 
-        if (command_arr.size() > 0) {
-            float s = start_time + i*0.01;
-            // std::cout << s << std::endl;
-
-            std::vector<float> c = command_arr.front();
-            command_arr.erase(command_arr.begin());
-
-            std::vector<uint8_t> buffer;
-
-            buffer = generate_command(c, s);
-
-            // for (uint8_t byte : buffer) {
-            //     std::cout << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte) << " ";
-            // }
-            // std::cout << std::endl;
-
-            ssize_t send_bytes = socketServerTX->send_(buffer);
-
-            i++;
-            
+        ssize_t received_bytes = socketServer->recv_(in_buffer);
+        if (received_bytes > 0) {
+            received_main_buffer += in_buffer;
         }
 
-        if (current_pose.queue_size > 0) {
+        if (check_buffer(received_main_buffer)) {
+            received_main_buffer.pop_back();
+            received_main_buffer.pop_back();
+            current_state = unpack(received_main_buffer);
+            current_joints.assign(current_state.values, current_state.values + numJoints);
+            received_main_buffer = "";
+            if (i == 0) {
+                command = current_joints;
+                i = 1;
+            }
+
+            if (command_arr.size() > 0) {
+                command = command_arr.front();
+                if (command_arr.size() > 1) command_arr.pop();
+                out_buffer = generate_command(command, false);
+                ssize_t sent_bytes = socketServer->send_(out_buffer);
+            }
+            else {
+                out_buffer = generate_command(command, false);
+                ssize_t sent_bytes = socketServer->send_(out_buffer);
+            }
+
             auto end = std::chrono::high_resolution_clock::now();
-            auto micro = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-            std::this_thread::sleep_for(std::chrono::microseconds((int)(1e4 - micro)));
+            auto dt = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+            std::this_thread::sleep_for(std::chrono::microseconds((int)period - dt));
+            // end = std::chrono::high_resolution_clock::now();
+            // auto loop_time = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+            // std::cout << loop_time / 1000.0 << std::endl;
+
+            start = std::chrono::high_resolution_clock::now();
+            if (dt > 1.0 / FREQ * 1.0e6) {
+                std::cerr << "late packet" << std::endl;
+                int idx = dt * FREQ / 1.0e6  - 1;
+                for (int k = 0; k < idx; k++) {
+                    if (command_arr.size() > 1) {
+                        command_arr.pop();
+                        std::vector<float> new_command(numJoints);
+                        float t = 1 / FREQ - dt;
+                        std::queue<std::vector<float>> command_arr_backup = command_arr;
+                        std::vector<float> first_command = command_arr_backup.front();
+                        command_arr_backup.pop();
+                        std::vector<float> second_command = command_arr_backup.front();
+                        for (int k = 0; k < numJoints; k++) {
+                            if (command_arr.size() >= 2) {
+                                new_command[k] = (first_command[k] * t + second_command[k] * (1.0 / FREQ - t)) * FREQ;
+                            }
+                        }
+                        command_arr.front() = new_command;
+                    }
+                }
+            }
         }
     }
 }
 
-int main(int argc, char **argv) {
+/*
+Main Function
+*/
+int main() {
+    std::thread main_t(main_thread);
+    std::thread user_input_t(user_input_thread);
 
-    std::thread listener(listener_thread);
-    std::thread setPose(settingPose_thread);
-    std::thread command(command_thread);
-
-    listener.join();
-    setPose.join();
-    command.join();
+    main_t.join();
+    user_input_t.join();
 
     return 0;
-
 }
