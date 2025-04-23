c393e5d459cc92ad6d1181dc9cdfe0b1ed98075f
Narek Bayanduryan <karim.hassan@inspire.ch>
2025-04-23 12:55:43 +0200

    ...

--- src/try.cpp
+++ src/try.cpp
@@ -8,12 +8,25 @@ void test(std::vector<float> vec) {
 }
 
 std::vector<float> current_joints;
+#define FREQ 1000
 
 int main() {
-    float x[6] = {0, 1, 10, 232, 2321, 123};
-    current_joints.assign(x, x+6);
-    for (float c : current_joints) {
-        std::cout << c << ", ";
+    // float x[6] = {0, 1, 10, 232, 2321, 123};
+    // current_joints.assign(x, x+6);
+    // for (float c : current_joints) {
+    //     std::cout << c << ", ";
+    // }
+    // std::cout << std::endl;
+
+    std::vector<int> a = {1, 2, 3, 4, 5, 6};
+
+    int idx = 0;
+
+    for (int k = 0; k < idx; k++) {
+        a.pop_back();
     }
-    std::cout << std::endl;
+
+    for (int s : a) {
+        std::cout << s << std::endl;
+    }
 }
